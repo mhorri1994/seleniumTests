5f44c78e241872e5e5209450ded8f0a886b8296d
mhorri1994 <m.horrigan@hotmail.co.uk>
2017-09-04 14:13:58 +0100

    Adding the first exercise

---
+++ src/test/java/usingJUnit.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 04/09/2017.
+ */
+public class usingJUnit {
+    @Test
+    public void myTestMethod(){
+        System.out.println("Hello World");
+    }
+}
