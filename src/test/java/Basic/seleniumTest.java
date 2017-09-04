529bc8500db6c1e5dac18124e6a6fe66574aa050
mhorri1994 <m.horrigan@hotmail.co.uk>
2017-09-04 14:27:21 +0100

    Updated hierarchy

--- src/test/java/Basic/seleniumTest.java
+++ src/test/java/Basic/seleniumTest.java
@@ -1,15 +1,16 @@
+package Basic;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 /**
  * Created by Administrator on 04/09/2017.
  */
-public class seleniumT {
-
+public class seleniumTest {
     WebDriver webDriver;
 
     @Before
@@ -17,13 +18,10 @@ public class seleniumT {
         webDriver = new ChromeDriver();
     }
 
-    @After
-    public void tearDown(){
-        webDriver.quit();
-    }
-
     @Test
     public void firstTest(){
-        webDriver.navigate().to("HTTP://www.qa.com");
+        webDriver.navigate().to("https://www.qa.com/");
+        Assert.assertEquals("https://www.qa.com/", webDriver.getCurrentUrl());
+        Assert.assertEquals("IT Training | Project Management Training | Business Skills Training | QA", webDriver.getTitle());
     }
 }
