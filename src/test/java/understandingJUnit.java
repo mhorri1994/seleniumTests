c7a8f68fc70c7ad45c1811a1032a001340af1db1
mhorri1994 <m.horrigan@hotmail.co.uk>
2017-09-04 14:16:20 +0100

    Adding the second exercise

---
+++ src/test/java/understandingJUnit.java
@@ -0,0 +1,28 @@
+import org.junit.*;
+
+/**
+ * Created by Administrator on 04/09/2017.
+ */
+public class understandingJUnit {
+    @BeforeClass
+    public static void beforeClass(){
+        System.out.println("BeforeClass");
+    }
+
+    @Before
+    public void before(){
+        System.out.println("Before");
+    }
+    @Test
+    public void test1(){
+        System.out.println("Test");
+    }
+    @After
+    public void after(){
+        System.out.println("After");
+    }
+    @AfterClass
+    public static void afterClass(){
+        System.out.println("AfterClass");
+    }
+}
