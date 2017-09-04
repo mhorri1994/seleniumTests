ff9400875f073f229d026ae72b466da780d56df7
mhorri1994 <m.horrigan@hotmail.co.uk>
2017-09-04 16:58:00 +0100

    Added ReportManager output files

---
+++ src/test/java/ExtentReportManager/ReportDetails.java
@@ -0,0 +1,22 @@
+package ExtentReportManager;
+
+import com.aventstack.extentreports.reporter.configuration.Theme;
+import lombok.Data;
+
+@Data
+public class ReportDetails {
+
+    private final String reportFilePath, documentTitle, reportName;
+
+    private Theme theme;
+
+    public ReportDetails(String reportFilePath, String documentTitle, String reportName){
+        this.reportFilePath = reportFilePath;
+        this.documentTitle = documentTitle;
+        this.reportName = reportName;
+
+        System.out.println("Report created here: " + reportFilePath);
+    }
+
+
+}
